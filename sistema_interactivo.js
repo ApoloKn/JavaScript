6b1d57adb807ea644baa5f9134cd596d1e28e1fd
fabian beleño <fabianrobles321@outlook.com>
2025-06-17 17:10:23 -0500

    JS

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,17 +1,19 @@
+//Mensaje de bienvenida en la consola
 console.log("¡Binevenido al sistema interactivo de mensajes!");
 
-//solicitar datos de usuario
+//Solicitar datos de usuario
 let nombre = prompt("Ingrese su nombre, por favor");
 let edad = prompt("Ingrese su edad, por favor");
 
-//convertir edad a numero
+//Convertir edad a numero
 edad = parseInt(edad);
 
-//validar y mostrar mensajes
+//Condicionales
 if (isNaN(edad)) {
-    console.error("Error: Ingrese una edad valida en numeros, por favor");
-} else if (edad < 18) {
-    alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando!`);
-} else {
-    alert(`Hola ${nombre}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`);
-}
+    console.error("Error: Ingrese una edad valida en numeros, por favor");}
+    //validar y mostrar mensaje a menores de edad
+    else if (edad < 18) {
+    alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando!`);}
+    //validar y mostrar mensaje a mayores de edad
+    else {
+    alert(`Hola ${nombre}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`);}
