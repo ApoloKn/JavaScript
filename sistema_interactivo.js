33166e224d9d205b60be27c1f66942846ed10a5b
fabian beleño <fabianrobles321@outlook.com>
2025-06-17 19:43:27 -0500

    training M3S1

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,19 +1,37 @@
+function username(name) {
+    if (name.trim() === "") {
+        // Manejar caso de entrada vacía
+        console.error("Error: Ingrese un nombre, por favor");
+        alert("Error: Ingrese un nombre, por favor")
+        history.go(0);
+    }
+}
+
+function Y_old() {
+    let edad = prompt("Ingrese su edad, por favor");
+    //Convertir edad de cadena a entero
+    edad = parseInt(edad);
+    if (isNaN(edad)) {
+        console.error("Error: Ingrese una edad valida en numeros, por favor");
+        alert("Error: Ingrese una edad valida en numeros, por favor")
+        history.go(0);
+    }
+    //validar y mostrar mensaje a menores de edad
+    else if (edad < 18) {
+        alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando!`);
+    }
+    //validar y mostrar mensaje a mayores de edad
+    else {
+        alert(`Hola ${nombre}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`);
+    }
+}
+
+
 //Mensaje de bienvenida en la consola
 console.log("¡Binevenido al sistema interactivo de mensajes!");
 
 //Solicitar datos de usuario
 let nombre = prompt("Ingrese su nombre, por favor");
-let edad = prompt("Ingrese su edad, por favor");
+username(nombre)
 
-//Convertir edad a numero
-edad = parseInt(edad);
-
-//Condicionales
-if (isNaN(edad)) {
-    console.error("Error: Ingrese una edad valida en numeros, por favor");}
-    //validar y mostrar mensaje a menores de edad
-    else if (edad < 18) {
-    alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando!`);}
-    //validar y mostrar mensaje a mayores de edad
-    else {
-    alert(`Hola ${nombre}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`);}
+let edad = Y_old()
