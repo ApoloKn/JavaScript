0c00db8b104609647b99834e5f4919b3dd8d9ca5
fabian beleño <fabianrobles321@outlook.com>
2025-06-18 19:56:05 -0500

    new training

---
+++ js/sistema_interactivo.js
@@ -0,0 +1,45 @@
+function username(name) {
+    
+    if (name.trim() === "") {
+        alert("Por favor, completa todos los campos.");
+        return;
+    }
+}
+
+function Y_old(age) {
+    
+    age = parseInt(age);
+    if (isNaN(age)) {
+        console.error("Error: Ingrese una edad valida en numeros, por favor");
+        alert("Error: Ingrese una edad valida en numeros, por favor")
+        history.go(0);
+    }
+    else if (age < 18) {
+        alert(`Hola ${name}, eres menor de edad. ¡Sigue aprendiendo y disfrutando!`);
+    }
+    else if (age <0) {
+        alert("Error: no puedes tener edades negativas")
+        history.go(0);
+    }
+    else {
+        alert(`Hola ${name}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`);
+    }
+}
+
+console.log("¡Binevenido al sistema interactivo de mensajes!");
+
+
+
+function login(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de forma tradicional
+
+    // Obtén los valores de los campos
+    let name = document.getElementById("name").value;
+    username(name)
+    let age = document.getElementById("age").value;
+    Y_old(age)
+
+    // Aquí puedes agregar lógica para validar los datos o enviarlos a un servidor
+    console.log("Nombre ingresado:", name);
+    console.log("Edad ingresada:", age);
+}
